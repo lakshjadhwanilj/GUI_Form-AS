f41a0ca7789e4b67a1c51d48bd5b6613f8d94791
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2020-10-26 18:20:33 +0530

    Remove Toast on select country.

--- app/src/main/java/com/example/contactform/MainActivity.java
+++ app/src/main/java/com/example/contactform/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getApplicationContext(),countries[position], Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
